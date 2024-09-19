3da9a72f317658037c6f05c3fd4c19adbb2f29a3
안기환 <ahngh0113@naver.com>
2024-09-19 16:25:05 +0900

    2-2. search 파일 SSG 적용

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,5 +1,4 @@
-import { ReactNode } from "react";
-import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
+import { ReactNode, useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import Link from "next/link";
 
@@ -9,37 +8,37 @@ import SearchableLayout from "@/components/searchable-layout";
 import MovieItem from "@/components/movie-item";
 
 import style from "./index.module.css";
+import { MovieData } from "@/typesc";
 
-export const getServerSideProps = async (
-  content: GetServerSidePropsContext
-) => {
-  const q = content.query.q;
+export default function Page() {
+  const [searchMovieDate, setSearchMovieDate] = useState<MovieData[]>([]);
 
-  const searchMovies = await fetchMovie(q as string);
-
-  return {
-    props: {
-      searchMovies,
-    },
-  };
-};
-
-export default function Page({
-  searchMovies,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   const router = useRouter();
-  const serachName = router.query.q as string;
+  const q = router.query.q;
 
-  if (searchMovies.length === 0) {
+  const getQueryMovieDate = async () => {
+    const searchMovies = await fetchMovie(q as string);
+
+    setSearchMovieDate(searchMovies);
+  };
+
+  useEffect(() => {
+    if (q) {
+      getQueryMovieDate();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [q]);
+
+  if (searchMovieDate.length === 0) {
     return (
       <div className={style.not_found}>
-        &quot;{serachName}&quot;에 대한 검색 결과가 없습니다.
+        &quot;{q}&quot;에 대한 검색 결과가 없습니다.
       </div>
     );
   }
   return (
     <div className={style.movie_items}>
-      {searchMovies.map((movie) => {
+      {searchMovieDate.map((movie) => {
         return (
           <Link
             href={`/movie/${movie.id}`}
